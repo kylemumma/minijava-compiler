49154b959377392077373aa375e03ac1c1f839b0
Ashwin Banwari <ash1152@cs.washington.edu>
2023-05-29 21:56:38 -0700

    aassing

--- src/runtime/test.s
+++ src/runtime/test.s
@@ -1,8 +1,9 @@
 Basic$$: .quad 0
 
 YoMama$$: .quad 0
-.quad YoMama$aa
+.quad YoMama$yummy
 .quad YoMama$potato
+.quad YoMama$get
 
 .text
 .globl asm_main
@@ -26,74 +27,52 @@ YoMama$potato:
 pushq %rbp
 movq %rsp, %rbp
 subq $16,%rsp
-movq 16(%rbp),%rax
-movq %rax,%rdi
-movq $2,%rax
-movq %rax,8(%rdi)
-movq 16(%rbp),%rax
-movq $-1,%rbx
-pushq %rbx
-pushq %rax
-movq (%rax),%rbx
-call *8(%rbx)
-popq %rdi
-popq %rdi
-movq %rax,%rdi
-call put
+movq $16,%rdi
+call mjcalloc
+leaq YoMama$$(%rip),%rdx
+movq %rdx,(%rax)
+movq %rax,-8(%rbp)
 movq $16,%rdi
 call mjcalloc
 leaq YoMama$$(%rip),%rdx
 movq %rdx,(%rax)
 movq %rax,-8(%rbp)
 movq -8(%rbp),%rax
-movq $-1,%rbx
-pushq %rbx
 pushq %rax
 movq (%rax),%rbx
+movq -8(%rbp),%rax
+pushq %rax
 call *8(%rbx)
 popq %rdi
 popq %rdi
-movq %rax,%rdi
-call put
+movq %rax,-8(%rbp)
+movq $0,%rax
+addq $16,%rsp
+popq %rbp
+ret
+
+YoMama$yummy:
+pushq %rbp
+movq %rsp, %rbp
+subq $0,%rsp
+movq 24(%rbp),%rax
+pushq %rax
 movq -8(%rbp),%rax
 movq $-1,%rbx
 pushq %rbx
 pushq %rax
 movq (%rax),%rbx
-call *8(%rbx)
+call *24(%rbx)
 popq %rdi
 popq %rdi
-movq %rax,%rdi
-call put
-movq 16(%rbp),%rax
-movq $-1,%rbx
-pushq %rbx
-pushq %rax
-movq (%rax),%rbx
-call *8(%rbx)
 popq %rdi
-popq %rdi
-movq %rax,%rdi
-call put
-movq $16,%rdi
-call mjcalloc
-leaq YoMama$$(%rip),%rdx
-movq %rdx,(%rax)
-movq $-1,%rbx
-pushq %rbx
-pushq %rax
-movq (%rax),%rbx
-call *8(%rbx)
-popq %rdi
-popq %rdi
-movq %rax,%rdi
-call put
-movq $5,%rax
-addq $16,%rsp
+movq %rax,8(%rdi)
+movq -8(%rbp),%rax
+addq $0,%rsp
 popq %rbp
 ret
 
-YoMama$aa:
+YoMama$get:
 pushq %rbp
 movq %rsp, %rbp
 subq $0,%rsp
